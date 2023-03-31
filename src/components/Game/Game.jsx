cb47397090071771865ba8687a86dd19f60bfa75
lidiettes <hello@lidiettes.com>
2023-03-31 23:06:37 +0200

    build: play without challenge

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import dice1 from "../../assets/images/Dice1.png"
+import dice2 from "../../assets/images/Dice2.png"
+import dice3 from "../../assets/images/Dice3.png"
+import dice4 from "../../assets/images/Dice4.png"
+import dice5 from "../../assets/images/Dice5.png"
+import dice6 from "../../assets/images/Dice6.png"
+import styles from "./Game.module.css"
+
 
 const Game = () => {
-  return (
-    <div>Game</div>
-  )
+
+    const diceImages = [
+        dice1,
+        dice2,
+        dice3,
+        dice4,
+        dice5,
+        dice6
+    ]
+
+    const [image, setNewImage] = useState(diceImages[0])
+    const [image2, setNewImage2] = useState(diceImages[1])
+
+    const rollDice = ()=>{
+        const randomNumber = Math.floor(Math.random() * 6) + 1;
+        const randomNumber2 = Math.floor(Math.random() * 6) + 1;
+        setNewImage(diceImages[randomNumber-1])
+        setNewImage2(diceImages[randomNumber2-1])
+    }
+
+    return (
+        <>
+            <div>Game</div>
+            <div className={styles.container}>
+                <img className={styles.square} src={image}/>
+                <div style={{ width: '5px', display: 'inline-block' }}></div>
+                <img className={styles.square} src={image2} />
+                <button onClick={rollDice}>Play</button>
+            </div>
+        </>
+    )
 }
 
 export default Game
--- src/pages/DicePage.jsx
+++ src/pages/DicePage.jsx
@@ -1,46 +1,14 @@
 import React, { useState } from 'react'
+import Game from '../components/Game/Game'
 
 const DicePage = () => {
-    const [rollResults, setRollResults] = useState(new Array(numberOfDice).fill(0));
-    const [finishedRolls, setFinishedRolls] = useState(false);
-
-    const rollDice = () => {
-        const newRollResults = rollResults.map(() => Math.floor(Math.random() * 6) +
-            1);
-        setRollResults(newRollResults);
-        setFinishedRolls(true);
-    }
-    const resetDice = () => {
-        setRollResults(new Array(numberOfDice).fill(0));
-        setFinishedRolls(false);
-    }
-    const toggleDice = (index) => {
-        const newRollResults = [...rollResults];
-        newRollResults[index] = newRollResults[index] === 0 ? 1 : 0;
-        setRollResults(newRollResults);
-    }
-    const dice = rollResults.map((result, index) => (
-        <Die
-            key={index}
-            index={index}
-            result={result}
-            finishedRolls={finishedRolls}
-            toggleDice={toggleDice}
-        />
-    ));
 
 
 
     return (
         <>
-            <div className="dice-container"></div>
-            <div className="dice-container">
-                <div className="dice-row">
-                    {dice}
+            <Game />
 
-                </div>
-            </div>
-        
         </>
 
     )
