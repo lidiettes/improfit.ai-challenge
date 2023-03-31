9383b073cab57515ed82670636babe52fa577446
lidiettes <hello@lidiettes.com>
2023-03-31 21:57:09 +0200

    build: open folders

--- src/pages/RandomPage.jsx
+++ src/pages/RandomPage.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import ButtonNext from '../components/ButtonNext/ButtonNext';
 
 const RandomPage = () => {
-  return (
-    <div>Numero ramdon </div>
-  )
+
+    const [targetScore, setTargetScore] = useState(0);
+    const randomNumber = Math.floor(Math.random() * 12) + 1;
+
+    return (
+        <>
+            <div>{randomNumber}</div>
+
+
+
+            <Link to="/dice">
+                <ButtonNext />
+            </Link>
+        </>
+    )
 }
 
 export default RandomPage
